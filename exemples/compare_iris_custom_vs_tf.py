0c2f5c312d4340fd9d8f301dfc12c4a6cca733cf
Ligny <antoine.deligny@epitech.eu>
2025-03-30 15:05:53 +0200

    [UPDATE] multiclass iris fixes

--- exemples/compare_iris_custom_vs_tf.py
+++ exemples/compare_iris_custom_vs_tf.py
@@ -6,7 +6,6 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelBinarizer
 
-# Import depuis le package neural_network
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from neural_network.layers import LayerDense, LayerSoftmaxCrossEntropy, BatchNormalization
 from neural_network.activations import ActivationReLU
@@ -18,10 +17,12 @@ from tensorflow.keras import layers
 
 def get_custom_model(input_dim: int, output_dim: int) -> NeuralNetwork:
     return NeuralNetwork([
-        LayerDense(input_dim, 16, activation=ActivationReLU()),
+        LayerDense(input_dim, 16),
         BatchNormalization(16),
-        LayerDense(16, 8, activation=ActivationReLU()),
+        ActivationReLU(),
+        LayerDense(16, 8),
         BatchNormalization(8),
+        ActivationReLU(),
         LayerDense(8, output_dim),
         LayerSoftmaxCrossEntropy(output_dim, output_dim)
     ])
@@ -32,10 +33,6 @@ def evaluate_custom(network, X_test, y_test) -> float:
         predicted = network.forward(inputs)
         predicted_label = np.argmax(predicted)
         expected_label = np.argmax(expected)
-
-        print(f"Expected: {expected}, Predicted: {predicted}")
-        print(f"Expected Label: {expected_label}, Predicted Label: {predicted_label}")
-
         correct += int(predicted_label == expected_label)
     return correct / len(y_test)
   
@@ -58,10 +55,10 @@ if __name__ == "__main__":
     custom_network = get_custom_model(X_train.shape[1], y_train.shape[1])
     dataset = list(zip(X_train, y_train))
 
-    optimizer = AdamOptimizer(learning_rate=0.001, clip_value=5.0, decay=1e-4)
+    optimizer = AdamOptimizer(learning_rate=0.001, clip_value=1.0, decay=1e-4)
 
     start_time = time.time()
-    custom_network.train(dataset, epochs=70, optimizer=optimizer, batch_size=32)
+    custom_network.train(dataset, epochs=10000, optimizer=optimizer, batch_size=32)
     custom_train_time = time.time() - start_time
 
     custom_accuracy = evaluate_custom(custom_network, X_test, y_test)
@@ -72,7 +69,7 @@ if __name__ == "__main__":
     sample = X_test[:1]
     true_label = np.argmax(y_test[:1])
 
-    predicted = custom_network.forward(sample[0])
+    predicted = custom_network.forward(sample)
     softmax_probs = softmax(predicted)
 
     predicted_label = np.argmax(softmax_probs)
